1bd90539b11494a0cc4b09d8ada1c12e45b79fad
Dilmith Wathsala <99314538+IT21334306@users.noreply.github.com>
2024-05-04 03:33:56 +0530

    updated Footer

--- e-frontend/e-frontend/src/components/footer/footer.js
+++ e-frontend/e-frontend/src/components/footer/footer.js
@@ -3,54 +3,60 @@ import './footer.css';
 import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
 import InstagramIcon from '@mui/icons-material/Instagram';
 
-const Footer=()=> {
-        return (
-            <div className="footer">
-                <div className="sb_footer section_padding">
-                    <div className="sb_footer-links">
-                        <div className="sb_footer-links-div">
-                            <h4>Resources</h4>
-                            <a href="/">
-                                <p>Home</p>
-                            </a>
-                            <a href="/customer_menu">
-                                <p>Packages</p>
-                            </a>
-                            <a href="/aboutUs">
-                                <p>Offers</p>
-                            </a>
-                            <a href="/event">
-                                <p>Day Tours</p>
-                            </a>
-                            <a href="/cus_feedback_add">
-                                <p>More</p>
-                            </a>
-                        </div>
-                        <div className="sb_footer-links-div">
-                            <h4>Contact Us</h4>
-                            <p>Tel: 011-2345678</p>
-                        </div>
-                        <div className="sb_footer-links-div">
-                            <h4>Coming soon on</h4>
-                            <div className="social-names">
-                                <a href="https://www.facebook.com/" className="links"><FacebookOutlinedIcon /></a>
-                                <a href="https://www.instagram.com/" className="links"><InstagramIcon /></a>
-                            </div>
-                        </div>
+const Footer = () => {
+    return (
+        <section id="footer">
+            <div className="container">
+                <div className="row text-center text-xs-center text-sm-left text-md-left">
+                    <div className="col-xs-12 col-sm-4 col-md-4">
+                        <h5>Quick links</h5>
+                        <ul className="list-unstyled quick-links">
+                            <li><a href="#"><i className="fa fa-angle-double-right"></i> Home</a></li>
+                            <li><a href="#"><i className="fa fa-angle-double-right"></i> Packages</a></li>
+                            <li><a href="#"><i className="fa fa-angle-double-right"></i> Offers</a></li>
+                            <li><a href="#"><i className="fa fa-angle-double-right"></i> Day Tours</a></li>
+                            <li><a href="#"><i className="fa fa-angle-double-right"></i> Feedback</a></li>
+                        </ul>
                     </div>
-                    <hr></hr>
-                    <div className="sb_footer-below">
-                        <div className="sb_footer-copyright">
-                            <p>
-                                &copy;{new Date().getFullYear()} Explore Lanka. All rights reserved.
-                            </p>
-                        </div>
-                        
+                    <div className="col-xs-12 col-sm-4 col-md-4">
+                        <h5>Contact Us</h5>
+                        <ul className="list-unstyled quick-links">
+                            <li><a href="#"><i className="fa fa-angle-double-right"></i> E-mail : explorelanka@gmail.com</a></li>
+                            <li><a href="#"><i className="fa fa-angle-double-right"></i>Phone : 0112 586 1234 </a></li>
+                            <li><a href="#"><i className="fa fa-angle-double-right"></i> Reviews</a></li>
+                            <li><a href="#"><i className="fa fa-angle-double-right"></i> Whatsapp : 070 3456545</a></li>
+                            <li><a href="#"><i className="fa fa-angle-double-right"></i> Expert Team</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-xs-12 col-sm-4 col-md-4">
+                        <h5>Quick links</h5>
+                        <ul className="list-unstyled quick-links">
+                            <li><a href="#"><i className="fa fa-angle-double-right"></i> Home</a></li>
+                            <li><a href="#"><i className="fa fa-angle-double-right"></i> About</a></li>
+                            <li><a href="#"><i className="fa fa-angle-double-right"></i> FAQ</a></li>
+                            <li><a href="#"><i className="fa fa-angle-double-right"></i> Get Started</a></li>
+                            <li><a href="#"><i className="fa fa-angle-double-right"></i> Contact</a></li>
+                        </ul>
                     </div>
                 </div>
+                <div className="row">
+                    <div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+                        <ul className="list-unstyled list-inline social text-center">
+                            <li className="list-inline-item"><a href="#"><FacebookOutlinedIcon /></a></li>
+                            <li className="list-inline-item"><a href="#"><InstagramIcon /></a></li>
+                        </ul>
+                    </div>
+                    <hr />
+                </div>
+                <div className="row">
+                    <div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+                        <p className="h6">2021 ? All right Reversed. <a className="text-green ml-2" href="#" target="_blank">JavaTpoint</a></p>
+                    </div>
+                    <hr />
+                </div>
             </div>
-        )
+        </section>
+    )
 }
-
 
 export default Footer;
